aac13efb04767656dd60b910174ae371f84473aa
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-30 22:20:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index {|dwarf, number| puts (number + 1).to_s + ".*" + dwarf}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteer_calls = []
+
+  planeteer_calls = planeteers.collect {|planeteer| (planeteer + "!").capitalize}
+
+  return planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  if planeteer_calls.find {|call| call.length > 4} then
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  found_index = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  for i in 0...cheese_types.length
+    found_index = array.find {|item| item == cheese_types[i]}
+    if found_index then
+      break
+    end
+  end
+
+  return found_index
 end
