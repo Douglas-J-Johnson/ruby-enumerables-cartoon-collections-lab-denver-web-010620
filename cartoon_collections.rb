44f212fd2056408bfa43b157b9b21f38c21f1e02
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-30 22:03:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 def summon_captain_planet(planeteers)
   planeteer_calls = []
 
-  planeteers.collect {|planeteer| (planeteer + "!").capitalize}
+  planeteer_calls = planeteers.collect {|planeteer| (planeteer + "!").capitalize}
 
   return planeteer_calls
 end
